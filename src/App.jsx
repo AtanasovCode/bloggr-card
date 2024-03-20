40a232d91f0bace08e2b9922235ddf9e0eb72378
AtanasovCode <a_ljupce@yahoo.com>
2024-03-20 16:03:06 +0100

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-background w-screen h-screen text-white flex justify-center items-center">
+      <div className="font-bold p-12 bg-red-500 underline">Hello TailwindCSS!</div>
+    </div>
+  );
+}
+
+export default App;
